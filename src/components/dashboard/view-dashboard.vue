5f19f2079866153c37b43cb686377b4b4e4ea901
Vicky Valerian <vickyvalerian27@gmail.com>
2023-04-21 02:30:02 +0700

    dashboard view

---
+++ src/components/dashboard/view-dashboard.vue
@@ -0,0 +1,275 @@
+<template>
+
+    <v-main class="list">
+        <h1 class="page-custom-title">DASHBOARD</h1>
+        <v-layout justify>
+            <v-flex xs5>
+                <v-select v-model="form.bulan" :items="bulan_list" label="Bulan"></v-select>
+            </v-flex>
+            <v-spacer></v-spacer>
+            <v-flex xs6>
+                <v-select v-model="form.tahun" :items="tahun_list" label="Tahun"></v-select>
+            </v-flex>
+        </v-layout>
+        <v-layout justify wrap>
+            <v-flex md6 sm6 xs12 class="pr-2 pb-2">
+                <v-card>
+                    <v-card-title><b>Transaksi Pencucian</b></v-card-title>
+                        <apex-chart width="100%" type="bar" :options="pencucianChartOptions" :series="pencucianChart.series"></apex-chart>  
+                </v-card>
+            </v-flex>
+            <v-flex md6 sm6 xs12 class="pr-2 pb-2">
+                <v-card>
+                    <v-card-title><b>Transaksi Kedai</b></v-card-title>
+                        <apex-chart 
+                            width="100%" type="bar" :options="kedaiChartOptions" :series="kedaiChart.series">
+                        </apex-chart>  
+                </v-card>
+            </v-flex>
+        </v-layout>
+        <v-snackbar v-model="snackbar.snackbarNotif" :color="snackbar.color" timeout="3000" bottom>{{ snackbar.error_message }}</v-snackbar>
+    </v-main>
+
+</template>
+
+<style>
+    .page-custom-title{
+        margin: 24px 0px;
+        text-align: left;
+        color: black !important;
+    }
+
+    .data-table-icon{
+        padding: 0 5px !important;
+    }
+
+    .btn-add-data{
+        background-color: #316291 !important;
+        color: #FCFDEF !important;
+        padding: 15px !important;
+        text-align: center !important;
+        font-size: 12px !important;
+        border-radius: 15px !important;
+        margin-right: 24px !important;
+    }
+
+    .btn-confirm-delete{
+        background-color: #316291 !important;
+        color: #FCFDEF !important;
+        padding: 15px !important;
+        text-align: center !important;
+        font-size: 12px !important;
+        border-radius: 15px !important;
+    }
+
+    .btn-confirm-cancel{
+        background-color: #FCFDEF !important;
+        border: 1px solid #316291 !important;
+        color: #316291 !important;
+        padding: 15px !important;
+        text-align: center !important;
+        font-size: 12px !important;
+        border-radius: 15px !important;
+    }
+
+    .dialog-confirm-title{
+        background-color: #316291 !important;
+        font-size: 24px !important;
+    }
+
+    .dialog-confirm-text{
+        text-align: left !important;
+        color: black !important;
+        align-content: center !important;
+        padding-top: 20px !important;
+        font-size: 16px !important;
+    }
+
+    .dialog-confirm-content{
+        padding-top: 20px !important;
+    }
+</style>
+
+<script>
+
+export default {
+    name: 'dashboard-view',
+    data() {
+        return {
+            bulan_list: [
+                { text: "Januari", value: 1 },
+                { text: "Februari", value: 2 },
+                { text: "Maret", value: 3 },
+                { text: "April", value: 4 },
+                { text: "Mei", value: 5 },
+                { text: "Juni", value: 6 },
+                { text: "Juli", value: 7 },
+                { text: "Agustus", value: 8 },
+                { text: "September", value: 9 },
+                { text: "Oktober", value: 10 },
+                { text: "November", value: 11 },
+                { text: "December", value: 12 }
+            ],
+            tahun_list: [
+                { text: "2021", value: 2021 },
+                { text: "2022", value: 2022 },
+                { text: "2023", value: 2023 },
+                { text: "2024", value: 2024 },
+                { text: "2025", value: 2025 },
+            ],
+            snackbar: {
+                snackbarNotif: false,
+                color: '',
+                message: '',
+            },
+            form: {
+                bulan: new Date().getMonth() + 1,
+                tahun: new Date().getFullYear(),
+            },
+            pencucianChart: {
+                series: [{
+                    name: 'Jumlah Pencucian',
+                    data: [4, 1, 1],
+                }],
+            },
+            pencucianChartOptions: {
+                noData:{
+                    text: "Tidak Ada Data Tersedia",
+                    align: "center",
+                    verticalAlign: "middle",
+                    offsetX: 0,
+                    offsetY: 0,
+                },
+                chart: {
+                    toolbar: {
+                        show: false,
+                    }
+                },
+                labels: [],
+            },
+            kedaiChart: {
+                series: [{
+                    name: 'Jumlah Penjualan',
+                    data: [],
+                }],
+            },
+            kedaiChartOptions: {
+                noData:{
+                    text: "Tidak Ada Data Tersedia",
+                    align: "center",
+                    verticalAlign: "middle",
+                    offsetX: 0,
+                    offsetY: 0,
+                },
+                chart: {
+                    toolbar: {
+                        show: false,
+                    }
+                },
+                labels: [],
+            },
+        }
+    },
+    created(){
+        this.refreshChart();
+    },
+    methods: {
+        refreshChart(){
+            let urlPencucianChart = this.$api + '/chart/transaksi-pencucian?bulan=' + this.form.bulan + '&tahun=' + this.form.tahun;
+            let urlKedaiChart = this.$api + '/chart/transaksi-kedai?bulan=' + this.form.bulan + '&tahun=' + this.form.tahun;
+
+            this.getPencucianChart(urlPencucianChart);
+            this.getKedaiChart(urlKedaiChart);
+        },
+
+        getPencucianChart(url){
+            this.$http.get(url).then(response => {
+                this.filterPencucianChart(response.data);
+            });
+        },
+
+        getKedaiChart(url){
+            this.$http.get(url).then(response => {
+                this.filterKedaiChart(response.data);
+            });
+        },
+
+        filterPencucianChart(data){
+            this.pencucianChart.series.length = 0;
+            this.pencucianChart.series.data = [];
+            this.pencucianChartOptions.labels.length = 0;
+            
+            let isNull = true;
+            
+            let dataTotal = [];
+            data.forEach(element => {
+                let series = element.total;
+                let label = element.formatTgl;
+
+                dataTotal.push(series);
+                this.pencucianChartOptions.labels.push(label);
+
+                if(element.total !== 0) {
+                    isNull = false;
+                }
+            });
+
+            let insertData = {
+                name: 'Jumlah Pencucian',
+                data: dataTotal,
+            }
+            this.pencucianChart.series.push(insertData);
+
+            if (isNull === true) {
+                this.pencucianChart.series = [];
+                this.pencucianChartOptions.labels.length = 0;
+            }
+        },
+
+        filterKedaiChart(data){
+            this.kedaiChart.series.length = 0;
+            this.kedaiChartOptions.labels.length = 0;
+            let isNull = true;
+
+            let dataTotal = [];
+            data.forEach(element => {
+                let series = element.total;
+                let label = element.formatTgl;
+                
+                dataTotal.push(series);
+                this.kedaiChartOptions.labels.push(label);
+
+                if(element.total !== 0) {
+                    isNull = false;
+                }
+            });
+
+            let insertData = {
+                name: 'Jumlah Penjualan',
+                data: dataTotal,
+            }
+            this.kedaiChart.series.push(insertData);
+
+            if (isNull === true) {
+                this.kedaiChart.series = [];
+                this.kedaiChartOptions.labels.length = 0;
+            }
+        },
+    },
+    mounted(){
+        
+    },
+    watch: {
+        'form.bulan'(){
+            this.refreshChart();
+        },
+
+        'form.tahun'(){
+            this.refreshChart();
+        },
+    },
+    computed:{
+
+    },
+}
+</script>

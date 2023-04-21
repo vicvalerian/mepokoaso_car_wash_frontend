b635b75814457fbbfed5db6178347c383cac5f83
Vicky Valerian <vickyvalerian27@gmail.com>
2023-04-22 00:40:41 +0700

    enchance choose car view

--- src/components/transaksi_pencucian/choose-car-transaksi-pencucian.vue
+++ src/components/transaksi_pencucian/choose-car-transaksi-pencucian.vue
@@ -32,8 +32,10 @@
                                             <v-img contain width="100" height="250" :src="previewImageUrl == '' ? $baseUrl+'/storage/'+item.foto : previewImageUrl" style="object-fit:cover;"></v-img>
                                         </v-card-title>
                                         <v-card-text>
+                                            <b class="black--text" style="font-size: 16px">
                                             {{ item.nama }} <br>
                                             Rp{{ item.harga }}
+                                            </b>
                                         </v-card-text>
                                     </v-card>
                                     </div>
